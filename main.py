b90a534971a96f8d227e6327bfc9fcbdd95ad8d6
ZariTen <zariten0@outlook.com>
2026-01-31 01:05:07 -0300

    Adicionar processamento GPS silver

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from src.spark_session import get_spark_session
 from src.etl.bronze import run_bronze_layer
+from src.etl.silver import run_silver_layer
 
 def main():
     spark = get_spark_session()
     
     run_bronze_layer(spark)
+    run_silver_layer(spark)
     
-    print("\n--- Validação Rápida ---")
-    try:
-        spark.read.parquet("data/bronze/gps").show(5)
-        spark.read.parquet("data/bronze/linhas").show(5)
-        spark.read.parquet("data/bronze/mco").show(5)
-    except:
-        print("Tabela GPS ainda não criada.")
-
     spark.stop()
 
 if __name__ == "__main__":
--- src/etl/silver.py
+++ src/etl/silver.py
@@ -1,6 +1,48 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.functions import current_timestamp, lit, col, regexp_replace
 
-def run_silver_layer():
+def process_gps_to_silver(spark: SparkSession):
+    """
+    Processa os dados de GPS da camada Bronze para a camada Silver.
+    """
+    try:
+        # 1. Leitura
+        bronze_path = os.path.join("data/bronze", "gps")
+        df_bronze = spark.read.format("parquet").load(bronze_path)
+
+        # 2. Transformações
+        df_silver = df_bronze.withColumn("numero_veiculo", F.col("NV").cast("integer")) \
+                             .withColumn("latitude", regexp_replace(F.col("LT"), ",", ".").cast("double")) \
+                             .withColumn("longitude", regexp_replace(F.col("LG"), ",", ".").cast("double")) \
+                             .withColumn("timestamp", F.to_timestamp(F.col("HR"), "yyyyMMddHHmmss")) \
+                             .withColumn("data_dia", F.date_format(F.col("timestamp"), "yyyy-MM-dd")) \
+                             .withColumn("hora_dia", F.date_format(F.col("timestamp"), "HH:mm:ss")) \
+                             .withColumn("velocidade", F.col("VL").cast("integer")) \
+                             .withColumn("numero_linha", F.col("NL").cast("integer")) \
+                             .withColumn("direcao", F.col("DG").cast("integer")) \
+                             .withColumn("caminho", F.when(F.col("SV") == 1, "IDA").otherwise("VOLTA")) \
+                             .withColumn("distancia_percorrida", F.col("DT").cast("integer")) \
+                             .drop("LT", "LG", "HR", "timestamp", "SV", "_ingestion_timestamp", "_source_file", "NV", "VL", "NL", "DG", "DT", "EV")
+
+        # 3. Escrita formato Delta Lake
+        silver_path = os.path.join("data/silver", "gps")
+        df_silver.write.format("delta").mode("overwrite").save(silver_path)
+
+        print(f"GPS salvo na Silver: {silver_path}")
+
+    except Exception as e:
+        print(f"Falha no fluxo GPS para Silver: {e}")
+
+def process_bronze_to_silver(spark: SparkSession):
+    """
+    Processa todos os dados da camada Bronze para a camada Silver.
+    """
+    process_gps_to_silver(spark)
+
+def run_silver_layer(spark):
     """
     Executa o processo ETL para a camada Silver.
     """
-    pass
+    process_bronze_to_silver(spark)
