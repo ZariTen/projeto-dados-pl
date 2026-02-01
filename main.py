a909912e1d3275ed2c767b92eb6d7862f0af1d28
ZariTen <zariten0@outlook.com>
2026-01-31 22:06:02 -0300

    Pip freeze requirements

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+import logging
+
 from src.spark_session import get_spark_session
 from src.etl.ingestao_bronze import run_bronze_layer
 from src.etl.ingestao_silver import process_bronze_to_silver
 from src.etl.ingestao_gold import run_gold_layer
-import logging
+
 
 def main():
     logging.basicConfig(level=logging.INFO)
     spark = get_spark_session()
     logging.info("Iniciando o processamento ETL")
     
-    #run_bronze_layer(spark)
+    run_bronze_layer(spark)
     process_bronze_to_silver(spark)
-    #run_gold_layer(spark)
+    run_gold_layer(spark)
 
     logging.info("Processamento ETL concluído com sucesso.")
 
     spark.stop()
+
 
 if __name__ == "__main__":
     main()
